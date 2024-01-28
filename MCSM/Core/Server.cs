50427634f9af456f3a6187d746eb060064f2906b
Render <79672833+Ren-deRing@users.noreply.github.com>
2024-01-28 23:15:38 +0900

    🚀 First Commit

---
+++ MCSM/Core/Server.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCSM.Core
+{
+    public class Server
+    {
+        public string dir;
+        public float ramAmount;
+        public bool noGUI;
+
+
+        public Server(string dir)
+        {
+            this.dir = dir;
+        }
+
+        public void Create(bool ignoreNotEmpty)
+        {
+            if (!ignoreNotEmpty && Directory.GetFiles(dir).Length != 0)
+            {
+                throw new Exception("Selected directory isn't empty");
+            }
+        }
+    }
+
+    public class ServerBuilder
+    {
+        private Server server;
+        public ServerBuilder(string dir)
+        {
+            server = new Server(dir);
+        }
+
+        public ServerBuilder SetRAM(float ramAmount)
+        {
+            server.ramAmount = ramAmount;
+            return this;
+        }
+
+        public ServerBuilder SetNoGUI(bool noGUI)
+        {
+            server.noGUI = noGUI;
+            return this;
+        }
+
+        public Server Build()
+        {
+            return server;
+        }
+    }
+}
---
+++ MCSM/Pages/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using MCSM.Core;
+using Ookii.Dialogs.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MCSM.Pages
+{
+    /// <summary>
+    /// MainPage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string dir;
+
+            VistaFolderBrowserDialog vfbd = new() { RootFolder = Environment.SpecialFolder.MyComputer };
+
+            if (vfbd.ShowDialog() == true)
+            {
+                dir = vfbd.SelectedPath;
+            }
+            else return;
+
+            ServerBuilder builder = new(dir);
+            Server s = builder
+                .SetNoGUI(true)
+                .SetRAM(4f)
+                .Build();
+
+            MessageBox.Show(s.dir, "MCSM Core");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
